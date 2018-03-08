a8fc2a0d53e3f7055d155683427aa1a7074af61d
Marek <marwis95@o2.pl>
2018-03-08 12:29:52 +0100

    Init

---
+++ RSLogixConverter/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RSLogixConverter
+{
+    public partial class Form1 : Form
+    {
+        char lastChar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            lastChar = richTextBox1.Text[richTextBox1.Text.Length - 1];
+            
+
+        }
+    }
+}
