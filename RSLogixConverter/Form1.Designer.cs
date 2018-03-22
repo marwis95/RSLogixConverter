ca439ddcf13aca960d9e67609d7dbc53dd6ca6b8
Marek <marwis95@o2.pl>
2018-03-22 13:08:59 +0100

    Converting chars to RSLogix chars

--- RSLogixConverter/Form1.Designer.cs
+++ RSLogixConverter/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -49,11 +51,33 @@
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(444, 276);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
@@ -66,6 +90,8 @@
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- RSLogixConverter/Form1.cs
+++ RSLogixConverter/Form1.cs
@@ -28,13 +28,50 @@ namespace RSLogixConverter
                 Chr = richTextBox1.Text[i];
                 numChr = Chr;
 
-                richTextBox2.AppendText(numChr.ToString() + " ");
+                if (numChr == 261) richTextBox2.AppendText("$B9");
+                else if (numChr == 263) richTextBox2.AppendText("$E6");
+                else if (numChr == 281) richTextBox2.AppendText("$EA");
+                else if (numChr == 322) richTextBox2.AppendText("$B3");
+                else if (numChr == 324) richTextBox2.AppendText("$F1");
+                else if (numChr == 243) richTextBox2.AppendText("$F3");
+                else if (numChr == 347) richTextBox2.AppendText("$9C");
+                else if (numChr == 380) richTextBox2.AppendText("$BF");
+                else if (numChr == 378) richTextBox2.AppendText("$9F");
 
-                //MessageBox.Show(numChr.ToString());
+                else if (numChr == 260) richTextBox2.AppendText("$A5");
+                else if (numChr == 262) richTextBox2.AppendText("$C6");
+                else if (numChr == 280) richTextBox2.AppendText("$CA");
+                else if (numChr == 321) richTextBox2.AppendText("$A3");
+                else if (numChr == 323) richTextBox2.AppendText("$D1");
+                else if (numChr == 211) richTextBox2.AppendText("$D3");
+                else if (numChr == 346) richTextBox2.AppendText("$8C");
+                else if (numChr == 379) richTextBox2.AppendText("$AF");
+                else if (numChr == 377) richTextBox2.AppendText("$8F");
+
+                else if (numChr == 10) richTextBox2.AppendText("$R$L");
+
+                else richTextBox2.AppendText(Chr.ToString());
+
+                //richTextBox2.AppendText(numChr.ToString() + " ");
 
 
             }
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            fd.Font = richTextBox1.SelectionFont;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fd.Font;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
         }
     }
 }
