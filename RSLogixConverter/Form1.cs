6a5ea3740c44b1248a3589a0fd86a7e39b6fbe4b
Marek <marwis95@o2.pl>
2018-04-10 10:27:12 +0200

    Modyfied encoding string

--- RSLogixConverter/Form1.cs
+++ RSLogixConverter/Form1.cs
@@ -46,36 +46,32 @@ namespace RSLogixConverter
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
+            bool finded = false;
             richTextBox2.Clear();
             for (int i = 0; i < (richTextBox1.Text.Length); i++)
             {
+                finded = false;
 
                 Chr = richTextBox1.Text[i];
                 numChr = Chr;
 
-                if (numChr == 261) richTextBox2.AppendText("$B9");
-                else if (numChr == 263) richTextBox2.AppendText("$E6");
-                else if (numChr == 281) richTextBox2.AppendText("$EA");
-                else if (numChr == 322) richTextBox2.AppendText("$B3");
-                else if (numChr == 324) richTextBox2.AppendText("$F1");
-                else if (numChr == 243) richTextBox2.AppendText("$F3");
-                else if (numChr == 347) richTextBox2.AppendText("$9C");
-                else if (numChr == 380) richTextBox2.AppendText("$BF");
-                else if (numChr == 378) richTextBox2.AppendText("$9F");
 
-                else if (numChr == 260) richTextBox2.AppendText("$A5");
-                else if (numChr == 262) richTextBox2.AppendText("$C6");
-                else if (numChr == 280) richTextBox2.AppendText("$CA");
-                else if (numChr == 321) richTextBox2.AppendText("$A3");
-                else if (numChr == 323) richTextBox2.AppendText("$D1");
-                else if (numChr == 211) richTextBox2.AppendText("$D3");
-                else if (numChr == 346) richTextBox2.AppendText("$8C");
-                else if (numChr == 379) richTextBox2.AppendText("$AF");
-                else if (numChr == 377) richTextBox2.AppendText("$8F");
+                for (int j = 0; j < 19 ; j++)
+                {
+                    if (numChr.ToString() == tab[j, 0])
+                    {
+                        richTextBox2.AppendText(tab[j, 1]);
+                        finded = true;
+                    }
+                }
 
-                else if (numChr == 10) richTextBox2.AppendText("$R$L");
+                if (finded == false)
+                {
+                    richTextBox2.AppendText(Chr.ToString());
+                }
 
-                else richTextBox2.AppendText(Chr.ToString());
+                //if (numChr == 10) richTextBox2.AppendText("$R$L");
+                //else richTextBox2.AppendText(Chr.ToString());
 
 
 
